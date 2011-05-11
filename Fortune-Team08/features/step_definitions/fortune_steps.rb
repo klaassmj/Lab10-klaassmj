4eac31eeec6392f6cb652d76b8d18982f82cd2f9
Matt Klaasse <matt@matt-HP-EliteBook-8530w.(none)>
2011-05-10 22:49:44 -0400

    tests passing but not sure if tests are correct. may be final commit

--- Fortune-Team08/features/step_definitions/fortune_steps.rb
+++ Fortune-Team08/features/step_definitions/fortune_steps.rb
@@ -3,3 +3,8 @@ Then /^I should see a fortune$/ do
 	fortune.text.should match /^\n.*\n\s*$/
 end
 
+Then /^I should see the fortune with index$/ do
+	fortune = page.find(".fortune")
+	fortune.text.should match /^\n.*\n\s*$/
+end
+
